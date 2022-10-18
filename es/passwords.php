32b4878fa6d02c7a92784ae3cbf2af111792cc4e
Rafa Veiga <veiga@iessanclemente.net>
2022-10-18 11:24:08 +0200

    Traducción de mensajes español para Laravel 9

--- es/passwords.php
+++ es/passwords.php
@@ -18,5 +18,4 @@ return [
     'throttled' => 'Por favor espere antes de volver a intentarlo.',
     'token' => 'Este token de restablecimiento de contraseña es inválido.',
     'user' => 'No se ha encontrado un usuario con esa dirección de correo.',
-
 ];
