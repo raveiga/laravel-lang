b7f778b4ca84b11d1d77b51dd35028a0bfbda001
Rafa Veiga <veiga@iessanclemente.net>
2022-10-18 11:20:39 +0200

    Versión 9 de Laravel

--- es/auth.php
+++ es/auth.php
@@ -15,6 +15,5 @@ return [
 
     'failed' => 'Estas credenciales no coinciden con nuestros registros.',
     'password' => 'La contraseña proporcionada es incorrecta.',
-    'throttle' => 'Demasiados intentos de acceso. Por favor inténtelo de nuevo en :seconds segundos.',
-
+    'throttle' => 'Demasiados intentos de acceso. Por favor inténtelo de nuevo en :seconds segundos.'
 ];
